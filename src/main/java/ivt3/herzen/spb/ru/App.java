9f93cea9691f7d539961dca68aef50cf7d4f27de
Zhukova <nicole.jk.08@gmail.com>
2016-07-04 14:47:16 +0300

    1

--- src/main/java/ivt3/herzen/spb/ru/App.java
+++ src/main/java/ivt3/herzen/spb/ru/App.java
@@ -1,44 +1,44 @@
 package ivt3.herzen.spb.ru;
 /**
  * Выполнила Жукова Николь */
-class MyThread extends Thread {
+class TheFirstThread extends Thread {
     @Override
     public void run() {
         int i, n;
         boolean b = true;
         n = ((int) (Math.random() * 900000000 + 100000000));
-        for (i = 2;  i <= Math.sqrt(n) && b ; i++ ) {
-
-            try {
-                Thread.sleep(1000);
-            }
-            catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        for (i = 2; i <= Math.sqrt(n) && b; i++) {
+            System.out.println(i) ;
             if (n % i == 0) {
                 b = false;
+
             }
         }
-        if(b){
+        if (b) {
             System.out.println("Число " + n + " является простым");
-        }
-        else {
+        } else {
             System.out.println("Число " + n + " не является простым");
         }
     }
 }
-
-    public class App {
-        public static void main(String[] args) {
-            MyThread MySecondThread = new MyThread();
-            MySecondThread.start();
-            while (MySecondThread.isAlive()) {
-                System.out.println("Идет обработка...");
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+class TheSecondThread extends Thread{
+    @Override
+    public void run() {
+        Thread TheFirstThread = new TheFirstThread();
+        TheFirstThread.start();
+        while ( TheFirstThread.isAlive()) {
+            System.out.println("Идет обработка...");
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
+}
+public class App {
+    public static void main(String[] args) {
+        Thread TheSecondThread = new TheSecondThread();
+        TheSecondThread.start();
+    }
+}
